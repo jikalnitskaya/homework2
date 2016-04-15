fa0f9212e6a870fc40d989bd8b6f24232aed1d59
Julia Kalnitskaya <jikalnitskaya@gmail.com>
2016-04-15 12:26:29 +0300

    hw js 6

--- HW_JS_6/js/script.fancybox.js
+++ HW_JS_6/js/script.fancybox.js
@@ -1,34 +1,81 @@
+'use strict';
+
 (function ($){
     $.fn.fancybox = function(){
 
     	var $link = this;
-    	console.log ($link);
     	var $body = $('body');
     	var $modal;
     	var $overlay;
+    	var $arrowLeft;
+    	var $arrowRight;
+    	var $close;
+    	var $par;
+    	var $href;
+    	var $title;
+    	var $next;
+    	var $previous;
 
     	function showModal (e){
-    		var $target = event.target;
-    		console.log ($target);
-    		var $parent = $target.parent(a);
-    		console.log ($parent);
-    		var href = $link.attr('href');
-    		var title = $link.attr('title');
-    		$modal = $('<div class="fancybox_modal"><img src="./'+href+'"><h1>'+title+'</h1></div>');
+    		var $target = e.target;
+    		$par = $($target).parent();
+    		$href = $par.attr('href');
+    		$title = $par.attr('title');
+    		$modal = $('<div class="fancybox_modal"><img src="./'+$href+'"><h1>'+$title+'</h1></div>');
     	    $overlay = $('<div class="fancybox_overlay"></div>');
+    	    $arrowLeft = $('<div class="arrow_left"></div>');
+    	    $arrowRight = $('<div class="arrow_right"></div>');
+    	    $close = $('<div class="close"></div>');
     	    e.preventDefault();
     	    $body.append($overlay);
     	    $body.append($modal);
-    	    
+    	    $body.append($arrowLeft);
+    	    $body.append($arrowRight);
+    	    $body.append($close);
+    	    $overlay.one('click', removeModal);
+    	    $close.one('click', removeModal);
+    	    $arrowRight.on('click', getNext);
+    	    $arrowLeft.on('click', getPrev);
     	};
 
     	function removeModal (){
-    		$modal.hide();
-    		$overlay.hide();
+    		$modal.remove();
+    		$overlay.remove();
+    		$arrowLeft.remove();
+    		$arrowRight.remove();
+    		$close.remove();
     	};
-//$(this).find('li').each(function(i)
+
+    	function getNext (){
+            $next = $($par).next('a');
+            $href = $next.attr('href');
+            $title = $next.attr('title');
+            $modal.remove();
+            $modal = $('<div class="fancybox_modal"><img src="./'+$href+'"><h1>'+$title+'</h1></div>');
+            $body.append($modal);
+            $par = $next;
+            $overlay.one('click', removeModal);
+    	    $close.one('click', removeModal);
+    	};
+
+    	function getPrev (){
+            $previous = $($par).prev('a');
+            if (!$previous.length) {
+            	$previous = $($par).last('a');
+            };
+            console.log ($previous);
+            $href = $previous.attr('href');
+            console.log ($href);
+            $title = $previous.attr('title');
+            $modal.remove();
+            $modal = $('<div class="fancybox_modal"><img src="./'+$href+'"><h1>'+$title+'</h1></div>');
+            $body.append($modal);
+            $par = $previous;
+            $overlay.one('click', removeModal);
+    	    $close.one('click', removeModal);
+    	};
+
     	$link.on('click', showModal);
-    	$overlay.one('click', removeModal);
     	return this;
     }   	
 })(jQuery);
