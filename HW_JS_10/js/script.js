4cb728563ee8227f7fc2975724201a0f7839227b
Julia Kalnitskaya <jikalnitskaya@gmail.com>
2016-05-17 18:13:01 +0300

    two

--- HW_JS_10/js/script.js
+++ HW_JS_10/js/script.js
@@ -1,4 +1,17 @@
-$(function (){
-    $(selector).simpleAccordion(class, single);
-    $('elem').simpleAccordion('customClass',true||false);
-}); (jQuery;)
+$(function() {
+    $("#second").rlAccordion('single',{
+
+        rlAccordion: "rlAccordion",     // add class in the same level of a parent statement for avoid styles conflict
+        signTag: "<span></span>",       // html tag parent signs
+        titles: "h3",                   // html tag parent of minus and plus, this may replaced also for a class
+        titlesChild: "span",            // html child titles and parent signs
+        container: "div",               // html tag adjacent sibling of titles
+        childNum: 0,                    // number of the children start open
+        classOpen: "opened",            // add class to the titles option adjacent sibling
+        open: "&#x2b;",                 // unicode plus sign
+        close: "&#x2212;",              // unicode minus sign
+        rlOpen: "rl-open",              // class for a plus sign
+        rlClose: "rl-close"             // class for a minus sign
+
+    });
+});
