9bb115b76b824304d34f8339e40aadd81e320c5b
Julia Kalnitskaya <jikalnitskaya@gmail.com>
2016-06-06 10:45:21 +0300

    hw_js_11 part1

--- HW_JS_11/js/script.js
+++ HW_JS_11/js/script.js
@@ -1,103 +1,31 @@
-'use strict';
+function pow(a, b) {
+    result = a;
 
-$(function () {
-	let questions = [{
-        question: "Какого цвета трава?",
-        answers: ["красная", "зеленая", "фиолетовая"],
-        correct: "зеленая"
-    }, {
-        question: "Где живет мышка?",
-        answers: ["в норе", "в сыре", "в скворечнике"],
-        correct: "в норе"
-    }, {
-        question: "Как зовут крокодила?",
-        answers: ["Семен Семеныч", "Гена", "Вася"],
-        correct: "Гена"
-    }];
-    let profile = {
-		title : "Тест по программированию",
-		que : questions,
-		buttonCheck : "Проверить мои результаты",
-	    buttonReset : "Сбросить все"
-	};
-//сохраняем и достаем объект из localStorage
-	localStorage.clear;
-	let str = JSON.stringify (profile);
-	localStorage.setItem('data',str);
-	console.log (localStorage);
-	//console.log (str);
-	let obj = JSON.parse (str);
-	//console.log (obj);
+    if (b == 0) {
+    	result = 1;
+    } else {
 
-//рендерим html
-	profile = obj;
-	let html = $('#test').html();
-	let content = tmpl (html, {users : profile});
-	$('body').append(content);
-	//сброс результатов
-	function clear() {
-		$('input:checked').removeAttr('checked');
-	};
-	let reset = $('#reset');
-	reset.on('click', clear);
+		for (i = 1; i < b; i++) {
+		  	result *=a;
+		}
+    }
+    return result;
+}
 
-//прооверка правильности
-    let $correct = [2, 1, 2];
-    let $modal;
-    let $overlay;
-    let $body = $('body');
-    let ansNum;
-    
-    function isCorrect () {
-        let $answers = $('input:checked').map(function (indx, el) {
-            return $(el).val();
-        });
-        let count = 0;
-        for (let i of $answers) {
-            if (i == $correct[i]-1) {
-                count++;
-            };
-        };
-        console.log (count);
-        let text;
-        let color;
-        let background;
-        let text2;
-        ansNum = count;
-        text2 = `You have ${count} &nbsp;right answers`;
-        if (count==1) {
-        	text2 = `You have ${count} &nbsp;right answer`;
-        } else {
-        	text2 = `You have ${count} &nbsp;right answers`;
-        };
+var a = prompt ('Введите число', '');
+var b = prompt ('Введите степень числа', '');
+var result;
 
-        if (count==3) {
-            text = 'You are right!!'          
-            color = '#228B22';
-            background = '#7FFF00';
-        } else {
-            	text = 'You are wrong!!';
-            	color = 'red';
-            	background = 'darkred';
-            }
-        $modal = $(`<div class="modal"><p>${text}</p><p class="text2">${text2}</p></div>`);
-        $overlay = $('<div class="overlay"></div>');
-        $body.append($overlay);
-    	$body.append($modal);
-        $modal.css({'color':color, 'background':background});
-    	console.log (text);
-    	$overlay.one('click', removeModal);
-    };
+do {
+    if (b < 0) {
+	    alert ('Введите положительное число для степени числа');
+	    b = prompt ('Введите степень числа', '');  
+	    pow(a, b);
+	} else { 	
+    	pow(a, b);
+    }
 
-    function removeModal() {
-    	$modal.remove();
-    	$overlay.remove();
-    };
+} while (b < 0);
 
-    let check = $('#check');
-	check.on('click', isCorrect);
-});
 
-try {
-    module.exports = script;
-} catch (e) {}
+console.log (result);
