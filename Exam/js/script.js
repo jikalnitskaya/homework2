364e6a515f29054d070ca2e00ca749443a90b1b4
Julia Kalnitskaya <jikalnitskaya@gmail.com>
2016-06-21 21:25:45 +0300

    arrow next

--- Exam/js/script.js
+++ Exam/js/script.js
@@ -1,30 +1,32 @@
 $(function() {
-	var $arrowRight = $('.step_1');
-	console.log ($arrowRight);
-	$arrowRight.bind('click', slideRight);
+	var $arrowRight = $('.arrow_right');
+	$arrowRight.on('click', slideRight);
 
-	/*var $arrowLeft = $('.arrow_left');
-	console.log ($arrowLeft);
-	$arrowLeft.on('click', slideLeft);*/
+	var $arrowLeft = $('.arrow_left');
+	$arrowLeft.on('click', slideLeft);
 
 
     function slideRight (e) {
-    	//e.stopPropagation();
+    	//e.stopImmediatePropagation();
+    	console.log ("this", this);
+    	var i = $arrowRight.attr("data-title");
+    	console.log ("i", i);
     	//e.preventDefault();
         var target = e.target;
-        console.log (target);
+        console.log ("target", target);
 	    var stepContainer = $('.step_1');
-	    console.log (stepContainer);
+	    console.log ("stepContainer", stepContainer);
         //var $background = $stepContainer.css('background');
         //console.log ($background);
-        //var stepItem = 
+        var stepItem = $('.step_item');
+        console.log ("stepItem", stepItem);
 	    var $url = 'url(./img/desctop_images/step_2.png) no-repeat';
 	    stepContainer.css("background", $url);
 	};
 
-	/*function slideLeft (e) {
-    	e.preventDefault();
+	function slideLeft (e) {
+    	//e.preventDefault();
         var target = e.target;
-        console.log (target);
-    };*/
+        console.log ("target", target);
+    };
 });
