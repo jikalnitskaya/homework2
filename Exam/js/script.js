eb70d752a75cdf9d23523bc22bbe662ec1cea98a
Julia Kalnitskaya <jikalnitskaya@gmail.com>
2016-06-22 14:28:45 +0300

    arrows ready

--- Exam/js/script.js
+++ Exam/js/script.js
@@ -5,48 +5,45 @@ $(function() {
 	var $arrowLeft = $('.arrow_left');
 	$arrowLeft.on('click', slideLeft);
 
+	var htmlStep_1 = $(".1").html();
+	var htmlStep_2 = $(".2").html();
+	var htmlStep_3 = $(".3").html();
+	var htmlSteps = [htmlStep_1, htmlStep_2, htmlStep_3];
 
     function slideRight () {
-    	console.log ("this", $(this));
-        var $target = $(this);//$arrowRight;
-        console.log ("target", $target);
+        var $target = $(this);
     	var i = $target.attr("data-title");
-        console.log ("i before", i);
-    		i = ++i;	
+    	i = ++i;	
     	if (i == 4) {
     		i = 1
     	};
-    	console.log ("i after", i);
-  
-	    var stepContainer = $target.parent('div');
-	    console.log ("stepContainer", stepContainer);
-        var stepItem = $target.next('.step_item');
-        console.log ("stepItem", stepItem);
+  		//меняем бекграунд
+  		var stepContainer = $target.parent('div');
 	    var $url = 'url(./img/desctop_images/step_'+i+'.png) no-repeat';
 	    stepContainer.css("background", $url);
+	    //меняем текст
+        var nextItem = htmlSteps[i-1];
+        var stepItem = $target.next('.step_item').html(nextItem);
+        //меняем номер аттрибута
 	    $target.attr("data-title", i);
-	    console.log ("attr", $target.attr("data-title"));
 	};
 
 	function slideLeft (e) {
-		console.log ("this", this);
-    	var $target = $(this);//$arrowLeft;
-        console.log ("target", $target);
+    	var $target = $(this);
     	var i = $target.attr("data-title");
-    	console.log ("i before", i);
-    		i = --i;	
+    	i = --i;	
     	if (i == 0) {
     		i = 3
     	};
-    	console.log ("i after", i);
   
-	    var stepContainer = $target.parent('div');
-	    //console.log ("stepContainer", stepContainer);
-        var stepItem = $target.next('.step_item');
-        console.log ("stepItem", stepItem);
+	    //меняем бекграунд
+  		var stepContainer = $target.parent('div');
 	    var $url = 'url(./img/desctop_images/step_'+i+'.png) no-repeat';
 	    stepContainer.css("background", $url);
-	    $target.removeAttr("data-title").attr("data-title", i);
-	    console.log ("attr", $target.attr("data-title"));
+	    //меняем текст
+        var nextItem = htmlSteps[i-1];
+        var stepItem = $target.prev('.step_item').html(nextItem);
+        //меняем номер аттрибута
+	    $target.attr("data-title", i);
     };
 });
